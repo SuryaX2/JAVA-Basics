b8b3de25af3d38f02165dab92a95e471ce461f45
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-05-28 18:26:18 +0530

    java

--- OOPS/Threads/thread.java
+++ OOPS/Threads/thread.java
@@ -1,11 +1,11 @@
+package OOPS.Threads;
 import java.io.*;
-
 class ThreadJoining extends Thread {
     public void run() {
         for (int i = 0; i < 2; i++) {
             try {
                 Thread.sleep(300);
-                System.out.println("Current Thread   : " + Thread.currentThread().getName());
+                System.out.println("Current Thread: " + Thread.currentThread().getName());
             } catch (Exception ex) {
                 System.out.println("Exception has" + " been caught" + ex);
             }
@@ -13,16 +13,23 @@ class ThreadJoining extends Thread {
         }
     }
 }
-
 class thread {
     public static void main(String[] args) {
         ThreadJoining t1 = new ThreadJoining();
         ThreadJoining t2 = new ThreadJoining();
         t1.start();
-        System.out.println("Current Thread: " + Thread.currentThread().getName());
-        t1.join();
+        try {
+            System.out.println("Current Thread: " + Thread.currentThread().getName());
+            t1.join();
+        } catch (Exception ex) {
+            System.out.println("Exception has " + "been caught" + ex);
+        }
         t2.start();
-        System.out.println("Current Thread: " + Thread.currentThread().getName());
-        t2.join();
+        try {
+            System.out.println("Current Thread: " + Thread.currentThread().getName());
+            t2.join();
+        } catch (Exception ex) {
+            System.out.println("Exception has been" + " caught" + ex);
+        }
     }
 }
