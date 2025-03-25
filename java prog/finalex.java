dc395a2fd5296e9b9ea7c852f32a48c72da3abf1
Surya Sekhar Sharma <141203476+SuryaX2@users.noreply.github.com>
2025-03-25 16:56:06 +0530

    Refactor Java classes for improved structure and readability

--- java prog/finalex.java
+++ java prog/finalex.java
@@ -1,17 +1,13 @@
-class A
-{
-public static void main(String args[])
-{
-int a=10;
-System.out.println(a);
-}
+class A {
+    public static void main(String args[]) {
+        int a = 10;
+        System.out.println(a);
+    }
 }
 
-class B extends A
-{
-public static void main(String args[])
-{
-int a=1;
-System.out.println(a);
-}
+class B extends A {
+    public static void main(String args[]) {
+        int a = 1;
+        System.out.println(a);
+    }
 }
