ed61d944497103a3415421b190b8cd55a2989ecc
Surya Sekhar Sharma <141203476+SuryaX2@users.noreply.github.com>
2025-04-29 13:02:41 +0530

    Refactor Java classes for improved formatting and readability

--- java prog/Mcon.java
+++ java prog/Mcon.java
@@ -1,33 +1,29 @@
-class A
-{
-A()
-{
-System.out.println("constructor of class A");
+class A {
+    A() {
+        System.out.println("constructor of class A");
+    }
 }
+
+class B extends A {
+    int a;
+
+    B(int a) {
+        this.a = a;
+        System.out.println("constructor of class B " + a);
+    }
 }
-class B extends A
-{
-int a;
-B(int a)
-{
-this.a=a;
-System.out.println("constructor of class B "+a);
+
+class C extends B {
+    C() {
+        super(12);
+        System.out.println("constructor of class C");
+    }
 }
-}
-class C extends B
-{
-C()
-{
-super(12);
-System.out.println("constructor of class C");
-}
-}
-class Demo
-{
-public static void main(String args[])
-{
-C c=new C();
-B b=new B(15);
-A a=new A();
-}
+
+class Demo {
+    public static void main(String args[]) {
+        C c = new C();
+        B b = new B(15);
+        A a = new A();
+    }
 }
--- java prog/mainthr.java
+++ java prog/mainthr.java
@@ -1,11 +1,9 @@
-class M
-{
-public static void main(String args[])
-{
-Thread obj=Thread.currentThread();
-System.out.println("Current Thread: "+obj);
-obj.setName("New Thread");
-obj.setPriority(Thread.MIN_PRIORITY);
-System.out.println("Current Thread: "+obj);
-}
+class M {
+    public static void main(String args[]) {
+        Thread obj = Thread.currentThread();
+        System.out.println("Current Thread: " + obj);
+        obj.setName("New Thread");
+        obj.setPriority(Thread.MIN_PRIORITY);
+        System.out.println("Current Thread: " + obj);
+    }
 }
